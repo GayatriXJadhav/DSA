15204366ecd9f3db249c88da42dcbc8bb1c19148
Gayatri Krishna Jadhav <gayatrijadhav1202@gmail.com>
2026-04-30 20:16:00 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (21.33%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -2,28 +2,26 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-         vector<int> front(n, 0);
-         vector<int> cur(n, 0);
+        vector<vector<int>> dp(n, vector<int>(n, 0));
 
         // Initialize the last row of dp with triangle values
         for (int j = 0; j < n; j++) {
-           front[j] = triangle[n - 1][j];
+            dp[n - 1][j] = triangle[n - 1][j];
         }
 
         // Traverse from second-last row to the top
         for (int i = n - 2; i >= 0; i--) {
             for (int j = i; j >= 0; j--) {
                 // Calculate sum from down and diagonal paths
-                int down = triangle[i][j] + front[j];
-                int diag = triangle[i][j] + front[j + 1];
+                int down = triangle[i][j] + dp[i + 1][j];
+                int diag = triangle[i][j] + dp[i + 1][j + 1];
 
                 // Store the minimum of the two paths
-                cur[j] = min(down, diag);
+                dp[i][j] = min(down, diag);
             }
-            front = cur;
         }
 
         // Return the minimum path sum from top
-        return front[0];
+        return dp[0][0];
     }
 };
